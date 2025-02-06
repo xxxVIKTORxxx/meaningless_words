576aa2908421d290ca9e4b38b9705e6d68d5e5bc
Viktor Ivanenko <109345462+xxxVIKTORxxx@users.noreply.github.com>
2025-02-06 15:55:35 +0300

    Add files via upload

--- meaningless.py
+++ meaningless.py
@@ -2,7 +2,7 @@ import random
 import string
 
 mu = 5.1
-sigma = 4.1
+sigma = 2.1
 
 letters = []
 for letter in string.ascii_lowercase:
@@ -16,11 +16,12 @@ def meaningless_word():
     word_random_lenght = int(random.gauss(mu, sigma))
     
     random_word = str()
-    for letter_random in range(1, word_random_lenght):
+    for letter_random in range(word_random_lenght):
         rand_let = random.choice(letters)
         random_word += rand_let
 
     return random_word
 
-print(meaningless_word())
+for n in range(10):
+    print(f'{meaningless_word()}')
 
